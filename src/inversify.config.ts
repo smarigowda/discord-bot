4a3df8a76e8552aea9af1e0026e5e06d1c35da1c
Santosh Marigowda <santosharakere@gmail.com>
2020-11-08 06:21:57 +0000

    using interfaces in inversify config

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -3,18 +3,19 @@ import { Container } from "inversify";
 import { Client } from "discord.js";
 
 import { Bot } from "./bot";
-import { PingFinder } from "./services/ping-finder";
 import { TYPES } from "./types";
+import { PingFinder } from "./services/ping-finder";
 import { MessageResponder } from "./services/message-responder";
+import { IMessageResponder, IPingFinder } from "./interfaces";
 
 let container = new Container();
 
 container.bind<Bot>(TYPES.Bot).to(Bot).inSingletonScope();
 container.bind<Client>(TYPES.Client).toConstantValue(new Client());
 container.bind<string>(TYPES.Token).toConstantValue(process.env.TOKEN);
-container.bind<PingFinder>(TYPES.PingFinder).to(PingFinder).inSingletonScope();
+container.bind<IPingFinder>(TYPES.PingFinder).to(PingFinder).inSingletonScope();
 container
-  .bind<MessageResponder>(TYPES.MessageResponder)
+  .bind<IMessageResponder>(TYPES.MessageResponder)
   .to(MessageResponder)
   .inSingletonScope();
 
